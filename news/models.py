a658a6d89897d19b65249ca0d0239240dbdfddb5
Abdurauf <AhmadjonovAbdurauf7@gmail.com>
2022-09-28 17:35:22 +0500

    for muallif

--- news/models.py
+++ news/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField()
+    muallif = models.CharField(max_length=100, default='Abdurauf')
 
     def __str__(self):
         return self.title
